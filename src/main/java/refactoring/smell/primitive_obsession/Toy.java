7c71e82eda9bc425f3fcf0925626eeb2a82e3398
Mei Xuesong <meixuesong@gmail.com>
2019-05-08 10:14:08 +0800

    add smell: primitive obsession

---
+++ src/main/java/refactoring/smell/primitive_obsession/Toy.java
@@ -0,0 +1,22 @@
+package refactoring.smell.primitive_obsession;
+
+public class Toy {
+    private String type;
+    private int length;
+    private String unit;
+
+    public Toy(String type, int length, String unit) {
+        this.type = type;
+        this.length = length;
+        this.unit = unit;
+    }
+
+
+    public int length() {
+        if ("m".equalsIgnoreCase(unit)) {
+            return length * 100;
+        }
+
+        return length;
+    }
+}
