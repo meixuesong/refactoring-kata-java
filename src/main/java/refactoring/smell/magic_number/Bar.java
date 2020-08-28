7dab2f6aa883fe0ee891ea6a096ad35e5706e897
Mei Xuesong <meixuesong@gmail.com>
2020-08-28 11:40:39 +0800

    update smell: magic number

--- src/main/java/refactoring/smell/magic_number/Bar.java
+++ src/main/java/refactoring/smell/magic_number/Bar.java
@@ -2,9 +2,15 @@ package refactoring.smell.magic_number;
 
 public class Bar {
 
+    /**
+     * 判断年龄是否达到18岁可饮酒年龄，以及状态是否有效
+     * @param age
+     * @param status：状态分为有效（1）和无效（0）
+     * @return
+     */
     public boolean doSomething(int age, int status) {
-        //Question: what does 21 and 1 means?
-        if (age > 21 && status == 1) {
+        //TODO
+        if (age > 18 && status == 1) {
             return true;
         } else {
             return false;
