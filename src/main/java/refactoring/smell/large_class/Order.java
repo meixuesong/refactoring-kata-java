b2f604288a1b05834d42ad516f54b445741a9334
Mei Xuesong <meixuesong@gmail.com>
2020-09-04 17:06:52 +0800

    refactor most smell codes

--- src/main/java/refactoring/smell/large_class/Order.java
+++
@@ -1,7 +0,0 @@
-package refactoring.smell.large_class;
-
-/**
- * @author  <a href="mailto:meixuesong@gmail.com">Mei Xuesong</a>
- */
-public class Order {
-}
--- src/main/java/refactoring/smell/long_method/readme.md
+++
@@ -1,3 +0,0 @@
-# Readme
-
-这是一个Kanban项目，待重构的方法是`KanbanService.query`.
